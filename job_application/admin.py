cabdef99e230b5ab9191bc271791d37f588cff89
louieeeee <louiesanqui0518@gmail.com>
2024-07-12 16:36:00 +0800

    Create a Base Template

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "date", "occupation")
+    search_fields = ("email", "occupation")
+    list_filter = ("date", "occupation")
+    ordering = ("first_name", )
+    readonly_fields = ("occupation", )
+
+
+admin.site.register(Form, FormAdmin)
--- job_application/views.py
+++ job_application/views.py
@@ -26,3 +26,5 @@ def index(request):
     return render(request, "index.html")
 
 
+def about(request):
+    return render(request, "about.html")
